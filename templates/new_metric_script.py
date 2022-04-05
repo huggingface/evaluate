a39370cb25842dbb0e69d8c90addcd0cdaf0bb3c
leandro <leandro.vonwerra@spoud.io>
2022-04-05 15:32:45 +0200

    replace datasets with evaluate in template

--- templates/new_metric_script.py
+++ templates/new_metric_script.py
@@ -46,7 +46,7 @@ Examples:
     Examples should be written in doctest format, and should illustrate how
     to use the function.
 
-    >>> my_new_metric = datasets.load_metric("my_new_metric")
+    >>> my_new_metric = evaluate.load_metric("my_new_metric")
     >>> results = my_new_metric.compute(references=[0, 1], predictions=[0, 1])
     >>> print(results)
     {'accuracy': 1.0}
@@ -56,13 +56,13 @@ Examples:
 BAD_WORDS_URL = "http://url/to/external/resource/bad_words.txt"
 
 
-@datasets.utils.file_utils.add_start_docstrings(_DESCRIPTION, _KWARGS_DESCRIPTION)
-class NewMetric(datasets.Metric):
+@evaluate.utils.file_utils.add_start_docstrings(_DESCRIPTION, _KWARGS_DESCRIPTION)
+class NewMetric(evaluate.Metric):
     """TODO: Short description of my metric."""
 
     def _info(self):
-        # TODO: Specifies the datasets.MetricInfo object
-        return datasets.MetricInfo(
+        # TODO: Specifies the evaluate.MetricInfo object
+        return evaluate.MetricInfo(
             # This is the description that will appear on the metrics page.
             description=_DESCRIPTION,
             citation=_CITATION,
