5116d2781fd1d0677015796bef1a8fc11d05ccec
leandro <leandro.vonwerra@spoud.io>
2022-04-11 15:25:45 +0200

    rename `dataset` to `evaluate` in src

--- src/evaluate/__init__.py
+++ src/evaluate/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-# Copyright 2020 The HuggingFace Datasets Authors and the TensorFlow Datasets Authors.
+# Copyright 2020 The HuggingFace Evaluate Authors and the TensorFlow Datasets Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
