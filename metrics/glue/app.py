d5b7659a9f520c9ba3e3dc9ce3513e93a792143d
leandro <leandro.vonwerra@spoud.io>
2022-05-27 13:57:44 +0200

    fix metric that require a config

--- metrics/glue/app.py
+++ metrics/glue/app.py
@@ -2,5 +2,5 @@ import evaluate
 from evaluate.utils import launch_gradio_widget
 
 
-module = evaluate.load("glue")
+module = evaluate.load("glue", "sst2")
 launch_gradio_widget(module)
--- metrics/indic_glue/app.py
+++ metrics/indic_glue/app.py
@@ -2,5 +2,5 @@ import evaluate
 from evaluate.utils import launch_gradio_widget
 
 
-module = evaluate.load("indic_glue")
+module = evaluate.load("indic_glue", "wnli")
 launch_gradio_widget(module)
