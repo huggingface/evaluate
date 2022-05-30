8faebaee71594c918498e3656c8a79b69f1e9efd
leandro <leandro.vonwerra@spoud.io>
2022-05-30 08:45:42 +0200

    fix namespace in `app.py`

--- metrics/rl_reliability/app.py
+++ metrics/rl_reliability/app.py
@@ -2,5 +2,5 @@ import evaluate
 from evaluate.utils import launch_gradio_widget
 
 
-module = evaluate.load("lvwerra/rl_reliability", "online")
+module = evaluate.load("rl_reliability", "online")
 launch_gradio_widget(module)
