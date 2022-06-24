f7dcd83c3e76bd77ccfdc569f1b661bd1af7ed9a
leandro <leandro.vonwerra@spoud.io>
2022-06-24 16:26:09 +0200

    fix module_name

--- src/evaluate/info.py
+++ src/evaluate/info.py
@@ -55,7 +55,7 @@ class EvaluationModuleInfo:
     module_type: str = "metric"
 
     # Set later by the builder
-    metric_name: Optional[str] = None
+    module_name: Optional[str] = None
     config_name: Optional[str] = None
     experiment_id: Optional[str] = None
 
