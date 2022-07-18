22b8c28d6b32971fb7b3c7914d7890d9d027a510
Felix Marty <felix@huggingface.co>
2022-07-18 21:10:18 +0200

    style

--- src/evaluate/evaluator/token_classification.py
+++ src/evaluate/evaluator/token_classification.py
@@ -122,8 +122,8 @@ class TokenClassificationEvaluator(Evaluator):
     def compute(
         self,
         model_or_pipeline: Union[
-            str, "Pipeline", Callable, "PreTrainedModel", "TFPreTrainedModel"
-        ] = None,  # noqa: F821
+            str, "Pipeline", Callable, "PreTrainedModel", "TFPreTrainedModel"  # noqa: F821
+        ] = None,
         data: Union[str, Dataset] = None,
         metric: Union[str, EvaluationModule] = None,
         tokenizer: Optional[Union[str, "PreTrainedTokenizer"]] = None,  # noqa: F821
