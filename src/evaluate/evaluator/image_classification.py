8356e9f883853d650ea640ad70e8e00ee3d2fd4d
Felix Marty <felix@huggingface.co>
2022-07-19 20:30:03 +0200

    fix warning bootstrap

--- src/evaluate/evaluator/image_classification.py
+++ src/evaluate/evaluator/image_classification.py
@@ -80,15 +80,16 @@ class ImageClassificationEvaluator(Evaluator):
         Examples:
         ```python
         >>> from evaluate import evaluator
-        >>> from datasets import Dataset, load_dataset
+        >>> from datasets import load_dataset
         >>> task_evaluator = evaluator("image-classification")
-        >>> data = load_dataset("beans", split="test[:2]")
+        >>> data = load_dataset("beans", split="test[:40]")
         >>> results = task_evaluator.compute(
         >>>     model_or_pipeline="nateraw/vit-base-beans",
         >>>     data=data,
         >>>     label_column="labels",
         >>>     metric="accuracy",
         >>>     label_mapping={'angular_leaf_spot': 0, 'bean_rust': 1, 'healthy': 2},
+        >>>     strategy="bootstrap"
         >>> )
         ```"""
 
