d781f85c55f94bd4f1896c12d9a2ea444f4805b3
Leandro von Werra <lvwerra@users.noreply.github.com>
2022-07-26 17:19:42 +0200

    Add comparisons and measurements to code quality tests (#204)
    * Add comparisons and measurements to code quality tests
    
    * fix measurement style
    
    * add measurment to isort in makefile
    
    * fix style

--- measurements/perplexity/app.py
+++ measurements/perplexity/app.py
@@ -2,5 +2,5 @@ import evaluate
 from evaluate.utils import launch_gradio_widget
 
 
-module = evaluate.load("perplexity", module_type= "measurement")
+module = evaluate.load("perplexity", module_type="measurement")
 launch_gradio_widget(module)
