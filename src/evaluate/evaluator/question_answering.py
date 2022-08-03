105248af082ed17a93073d51e98bc5fe139cde08
helen <helen@huggingface.co>
2022-08-03 14:27:30 -0700

    Add QA canary data and update evaluator

--- src/evaluate/evaluator/question_answering.py
+++ src/evaluate/evaluator/question_answering.py
@@ -12,10 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import hashlib
+import os
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+
+import dill
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 # Lint as: python3
 from datasets import Dataset, load_dataset
+
+from evaluate.utils import canary
 
 
 try:
@@ -118,6 +126,18 @@ class QuestionAnsweringEvaluator(Evaluator):
             result.append(pred)
         return {"predictions": result}
 
+    def compute_canary(self, pipe, input_columns, label_column):
+        canary_data = canary.CanaryDataset(self.task, input_columns, label_column)
+        _, canary_inputs = self.prepare_data(
+            data=canary_data.data,
+            question_column=input_columns[0],
+            context_column=input_columns[1],
+            id_column=input_columns[0],
+            label_column=label_column,
+        )
+        predictions, _ = self.call_pipeline(pipe, canary_inputs)
+        self.canary_hash = hashlib.md5(dill.dumps(predictions)).hexdigest()
+
     def compute(
         self,
         model_or_pipeline: Union[str, "Pipeline", Callable, "PreTrainedModel", "TFPreTrainedModel"] = None,
@@ -134,6 +154,7 @@ class QuestionAnsweringEvaluator(Evaluator):
         id_column: str = "id",
         label_column: str = "answers",
         squad_v2_format: Optional[bool] = None,
+        cache_if_possible=False,
     ) -> Tuple[Dict[str, float], Any]:
         """
         Compute the metric for a given pipeline and dataset combination.
@@ -252,6 +273,23 @@ class QuestionAnsweringEvaluator(Evaluator):
         if squad_v2_format:
             self.PIPELINE_KWARGS["handle_impossible_answer"] = True
 
+        # If `cache_if_possible` = True, test whether this exact pipe has been instantiated before
+        if cache_if_possible:  # TODO: also check that canaries have been implemented for this task type
+            input_columns = [question_column, context_column, id_column]
+            self.compute_canary(pipe, input_columns, label_column)
+
+            # Check if model, data, metric combination has already been computed and cached
+            cache_file_name = os.path.join(
+                self.cache_dir, f"cache-{self.canary_hash}-{data._fingerprint}-{metric._hash}" + ".parquet"
+            )
+
+            # Retrieve computed results from the cache if they already exist
+            if os.path.exists(cache_file_name):
+                logger.warning(f"Loading cached computed results at {cache_file_name}")
+                result_from_table = pa.Table.to_pydict(pq.read_table(cache_file_name))
+                result = {k: v[0] for (k, v) in result_from_table.items()}
+                return result
+
         predictions, perf_results = self.call_pipeline(pipe, **pipe_inputs)
         predictions = self.predictions_processor(predictions, squad_v2_format=squad_v2_format, ids=data[id_column])
 
@@ -269,5 +307,12 @@ class QuestionAnsweringEvaluator(Evaluator):
 
         result.update(metric_results)
         result.update(perf_results)
+
+        # Cache evaluation results.
+        #   These can be removed by calling evaluate.utils.file_utils.cleanup_cache_files(self.cache_dir)
+        if cache_if_possible:
+            logger.warning(f"Caching computed result to {cache_file_name}")
+            results_to_table = pa.Table.from_pydict({k: [v] for (k, v) in result.items()})
+            pa.parquet.write_table(results_to_table, cache_file_name)
 
         return result
--- src/evaluate/utils/canary.py
+++ src/evaluate/utils/canary.py
@@ -19,6 +19,20 @@ class CanaryDataset:
             data = load_dataset("beans", split="test")[:2]
             data[input_columns] = data.pop("image")
             data[label_column] = data.pop("labels")
+        elif evaluator_task == "question-answering":
+            data = {
+                "id": ["001", "002"],
+                "title": ["Ocean animals", "Whales"],
+                "context": [
+                    "A whale is bigger than every fish on the planet! it is bigger than the moon!!! whale!!",
+                    "finding a whale is like finding a needle in a haystack, except the needle is a whale and the haystack is the ocean",
+                ],
+                "question": ["Is a whale bigger than the moon??", "If the ocean is a haystack, what is a whale??"],
+                "answers": [
+                    {"text": ["yes", "yes", "yes"], "answer_start": [3, 3, 3]},
+                    {"text": ["A needle", "A needle", "A needle"], "answer_start": [7, 7, 7]},
+                ],
+            }
         else:
             raise ValueError(f"Canary dataset not implemented for this Evaluator task: {evaluator_task}!")
         # TODO: fill in canaries for other evaluators
