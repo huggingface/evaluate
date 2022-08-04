af3a3f289890dbc35e6235f1be4b64fc835eb95f
mathemakitten <helen@huggingface.co>
2022-08-04 15:51:30 -0700

    Do not return config object, and also pass in model from run() only

--- src/evaluate/harness/__init__.py
+++ src/evaluate/harness/__init__.py
@@ -28,8 +28,24 @@ class Harness:
     ```
     """
 
-    def __init__(self, config):
-        self.config = config
+    def __init__(self, path):
+        """ Instantiates a Harness object from a JSON file which will be passed to the Evaluator to run evaluation for a
+        model on this collection of tasks."""
+
+        filename = list(filter(lambda x: x, path.replace(os.sep, "/").split("/")))[-1]
+        if not filename.endswith(".json"):
+            filename = filename + ".json"
+        if path.endswith(filename):  # Try locally
+            if os.path.isfile(path):
+                json_filepath = path
+                self.config = json.load(open(json_filepath))
+            else:
+                raise FileNotFoundError(f"Couldn't find a configuration file at {relative_to_absolute_path(path)}")
+        # Load from a space on the Hub
+        elif is_relative_path(path) and path.count("/") <= 1:
+            json_filepath = cached_path(convert_hf_hub_path(os.path.join(path, "harness_config.json")))
+            self.config = json.load(open(json_filepath))
+
         self.tasks = []
         for task_group in self.config['task_groups']:
             for task in task_group['tasks']:
@@ -67,11 +83,10 @@ class Harness:
             for task in task_group["tasks"]:
                 logger.info(f"Running task: {task['name']}")
 
-                # data = Dataset.from_dict(load_dataset(task['data'])["test"][:])
-                data = Dataset.from_dict(load_dataset(task['data'])["test"][:10])
+                data = Dataset.from_dict(load_dataset(task['data'])["test"][:])
 
                 args_for_task = task["args_for_task"]
-                args_for_task["model_or_pipeline"] = self.config["model_or_pipeline"] or model_or_pipeline
+                args_for_task["model_or_pipeline"] = model_or_pipeline
                 args_for_task["data"] = data
 
                 results = e.compute(**args_for_task)
