13b619fe39f1d84a64187ed42c692eaa67d09141
sashavor <alexandra.vorobyova@gmail.com>
2022-08-23 09:21:21 -0400

    fixing loading

--- measurements/toxicity/toxicity.py
+++ measurements/toxicity/toxicity.py
@@ -125,18 +125,11 @@ class Toxicity(evaluate.Measurement):
 
     def _download_and_prepare(self, dl_manager):
         if self.config_name == "default":
+            logger.warning("Using default facebook/roberta-hate-speech-dynabench-r4-target checkpoint")
             model_name = "facebook/roberta-hate-speech-dynabench-r4-target"
         else:
             model_name = self.config_name
-            logger.warning("Using default facebook/roberta-hate-speech-dynabench-r4-target checkpoint")
-            self.toxic_classifier = pipeline(
-                "text-classification",
-                model="facebook/roberta-hate-speech-dynabench-r4-target",
-                top_k=99999,
-                truncation=True,
-            )
-        else:
-            self.toxic_classifier = pipeline("text-classification", model=self.config_name, top_k=2, truncation=True)
+        self.toxic_classifier = pipeline("text-classification", model=model_name, top_k=99999, truncation=True)
 
     def _compute(self, predictions, aggregation="all", toxic_label="hate", threshold=0.5):
         scores = toxicity(predictions, self.toxic_classifier, toxic_label)
