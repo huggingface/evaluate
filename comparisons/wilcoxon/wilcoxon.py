a4795b8c9a07e353d642697f52f76b2e76ed2697
leandro <leandro.vonwerra@spoud.io>
2022-08-26 12:20:29 +0200

    update all comparisons

--- comparisons/wilcoxon/wilcoxon.py
+++ comparisons/wilcoxon/wilcoxon.py
@@ -55,12 +55,13 @@ _CITATION = """
 
 @evaluate.utils.file_utils.add_start_docstrings(_DESCRIPTION, _KWARGS_DESCRIPTION)
 class Wilcoxon(evaluate.Comparison):
-    def _info(self):
+    def _info(self, config):
         return evaluate.ComparisonInfo(
             module_type="comparison",
             description=_DESCRIPTION,
             citation=_CITATION,
             inputs_description=_KWARGS_DESCRIPTION,
+            config=config,
             features=datasets.Features(
                 {
                     "predictions1": datasets.Value("float"),
