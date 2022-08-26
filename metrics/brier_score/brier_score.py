7782d44ed9878ffcfa784b0e5291a381b28cea7e
Kadir Nar <kadir.nar@hotmail.com>
2022-08-26 14:22:07 +0100

    Update metrics/brier_score/brier_score.py
    Co-authored-by: Leandro von Werra <lvwerra@users.noreply.github.com>

--- metrics/brier_score/brier_score.py
+++ metrics/brier_score/brier_score.py
@@ -71,8 +71,8 @@ Examples:
         >>> predictions = np.array([0.1, 0.9, 0.8, 0.3])
         >>> results = brier_score.compute(references=references, predictions=predictions)
         >>> print(round(results["brier_score"], 4))
-
         0.3375
+
     Example-2: if y_true contains string, an error will be raised and pos_label should be explicitly specified.
         >>> import numpy as np
         >>> brier_score = evaluate.load("brier_score")
