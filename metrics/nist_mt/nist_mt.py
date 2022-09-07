4f8ce3b728b411bf5c034725cf4d908d4a3187f0
Bram Vanroy <Bram.Vanroy@UGent.be>
2022-09-07 16:42:07 +0200

    fix trailing whitespace

--- metrics/nist_mt/nist_mt.py
+++ metrics/nist_mt/nist_mt.py
@@ -52,7 +52,7 @@ mteval-14.pl. The main differences are:
 
  - BLEU uses geometric mean of the ngram overlaps, NIST uses arithmetic mean.
  - NIST has a different brevity penalty
- - NIST score from mteval-14.pl has a self-contained tokenizer (in the Hugging Face implementation we rely on NLTK's 
+ - NIST score from mteval-14.pl has a self-contained tokenizer (in the Hugging Face implementation we rely on NLTK's
 implementation of the NIST-specific tokenizer)
 """
 
@@ -68,7 +68,7 @@ Args:
     n: highest n-gram order
     tokenize_kwargs: arguments passed to the tokenizer (see: https://github.com/nltk/nltk/blob/90fa546ea600194f2799ee51eaf1b729c128711e/nltk/tokenize/nist.py#L139)
 Returns:
-    'nist_mt': nist_mt score 
+    'nist_mt': nist_mt score
 Examples:
     >>> nist_mt = evaluate.load("nist_mt")
     >>> hypothesis = "It is a guide to action which ensures that the military always obeys the commands of the party"
