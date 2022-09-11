4e07652aecf880f8b02f621a0328a70b6b0e59e2
Bram Vanroy <Bram.Vanroy@UGent.be>
2022-09-11 11:37:08 +0200

    make style

--- metrics/charcut_mt/tests.py
+++ metrics/charcut_mt/tests.py
@@ -7,25 +7,35 @@ charcut = Charcut()
 
 @fixture
 def example_saudis():
-    return {"hyp": "this week the saudis denied information published in the new york times",
-            "ref": "saudi arabia denied this week information published in the american new york times",
-            "charcut_mt": 0.20915032679738563}
+    return {
+        "hyp": "this week the saudis denied information published in the new york times",
+        "ref": "saudi arabia denied this week information published in the american new york times",
+        "charcut_mt": 0.20915032679738563,
+    }
 
 
 @fixture
 def example_estimate():
-    return {"hyp": "this is in fact an estimate",
-            "ref": "this is actually an estimate",
-            "charcut_mt": 0.16363636363636364}
+    return {
+        "hyp": "this is in fact an estimate",
+        "ref": "this is actually an estimate",
+        "charcut_mt": 0.16363636363636364,
+    }
 
 
 @fixture
 def example_corpus():
-    return {"hyp": ["this week the saudis denied information published in the new york times",
-                    "this is in fact an estimate"],
-            "ref": ["saudi arabia denied this week information published in the american new york times",
-                    "this is actually an estimate"],
-            "charcut_mt": 0.1971153846153846}
+    return {
+        "hyp": [
+            "this week the saudis denied information published in the new york times",
+            "this is in fact an estimate",
+        ],
+        "ref": [
+            "saudi arabia denied this week information published in the american new york times",
+            "this is actually an estimate",
+        ],
+        "charcut_mt": 0.1971153846153846,
+    }
 
 
 def test_example_saudis(example_saudis):
