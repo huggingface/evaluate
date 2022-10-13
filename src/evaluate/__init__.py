2f6ae64b609673a595708ab67382b9b42516b6b9
leandro <leandro.vonwerra@spoud.io>
2022-10-13 14:58:35 +0200

    Release: 0.3.0

--- src/evaluate/__init__.py
+++ src/evaluate/__init__.py
@@ -17,7 +17,7 @@
 # pylint: enable=line-too-long
 # pylint: disable=g-import-not-at-top,g-bad-import-order,wrong-import-position
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 from packaging import version
 
