675804e8cb64154f308f3b182edc1087c0d89209
Kashif Rasul <kashif.rasul@gmail.com>
2022-10-28 12:51:43 +0200

    added mase

---
+++ metrics/mase/README.md
@@ -0,0 +1,129 @@
+---
+title: MASE
+emoji: 🤗 
+colorFrom: blue
+colorTo: red
+sdk: gradio
+sdk_version: 3.0.2
+app_file: app.py
+pinned: false
+tags:
+- evaluate
+- metric
+description: >-
+  Mean Absolute Scaled Error (MASE) is the mean absolute error of the forecast values, divided by the mean absolute error of the in-sample one-step naive forecast on the training set.
+---
+
+# Metric Card for MASE
+
+## Metric Description
+
+Mean Absolute Scaled Error (MASE) is the mean absolute error of the forecast values, divided by the mean absolute error of the in-sample one-step naive forecast:
+
+$$
+M = \frac{\sum_{i=1}^{n} |x_i - y_i| / n}{\sum_{t=p+1}^{T} |z_t - z_{t-p}/ (T-p)}.
+$$
+
+This metric is:
+*  independent of the scale of the data;
+* has predictable behavior when predicted/ground-truth data is near zero;
+*  symmetric;
+* interpretable,  as values greater than one indicate that in-sample one-step forecasts from the naïve method perform better than the forecast values under consideration.
+
+
+## How to Use
+
+At minimum, this metric requires predictions, references and training data as inputs.
+
+```python
+>>> mase_metric = evaluate.load("mase")
+>>> predictions = [2.5, 0.0, 2, 8]
+>>> references = [3, -0.5, 2, 7]
+>>> training = [5, 0.5, 4, 6, 3, 5, 2]
+>>> results = mase_metric.compute(predictions=predictions, references=references, training=training)
+```
+
+### Inputs
+
+Mandatory inputs: 
+- `predictions`: numeric array-like of shape (`n_samples,`) or (`n_samples`, `n_outputs`), representing the estimated target values.
+- `references`: numeric array-like of shape (`n_samples,`) or (`n_samples`, `n_outputs`), representing the ground truth (correct) target values.
+- `training`: numeric array-like of shape (`n_train_samples,`) or (`n_train_samples`, `n_outputs`), representing the in sample training data.
+
+Optional arguments:
+- `periodicity`: the seasonal periodicity of training data. The default is 1.
+- `sample_weight`: numeric array-like of shape (`n_samples,`) representing sample weights. The default is `None`.
+- `multioutput`: `raw_values`, `uniform_average` or numeric array-like of shape (`n_outputs,`), which defines the aggregation of multiple output values. The default value is `uniform_average`.
+  - `raw_values` returns a full set of errors in case of multioutput input.
+  - `uniform_average` means that the errors of all outputs are averaged with uniform weight. 
+  - the array-like value defines weights used to average errors.
+
+### Output Values
+This metric outputs a dictionary, containing the mean absolute error score, which is of type:
+- `float`: if multioutput is `uniform_average` or an ndarray of weights, then the weighted average of all output errors is returned.
+- numeric array-like of shape (`n_outputs,`): if multioutput is `raw_values`, then the score is returned for each output separately. 
+
+Each MASE `float` value ranges from `0.0` to `1.0`, with the best value being 0.0.
+
+Output Example(s):
+```python
+{'mase': 0.5}
+```
+
+If `multioutput="raw_values"`:
+```python
+{'mase': array([0.5, 1. ])}
+```
+
+#### Values from Popular Papers
+
+
+### Examples
+
+Example with the `uniform_average` config:
+```python
+>>> mase_metric = evaluate.load("mase")
+>>> predictions = [2.5, 0.0, 2, 8]
+>>> references = [3, -0.5, 2, 7]
+>>> training = [5, 0.5, 4, 6, 3, 5, 2]
+>>> results = mase_metric.compute(predictions=predictions, references=references, trianing=training)
+>>> print(results)
+{'mase': 0.1833...}
+```
+
+Example with multi-dimensional lists, and the `raw_values` config:
+```python
+>>> mase_metric = evaluate.load("mase", "multilist")
+>>> predictions = [[0.5, 1], [-1, 1], [7, -6]]
+>>> references = [[0.1, 2], [-1, 2], [8, -5]]
+>>> training = [[0.5, 1], [-1, 1], [7, -6]]
+>>> results = mase_metric.compute(predictions=predictions, references=references, training=training)
+>>> print(results)
+{'mase': 0.1818...}
+>>> results = mase_metric.compute(predictions=predictions, references=references, training=training, multioutput='raw_values')
+>>> print(results)
+{'mase': array([0.1052..., 0.2857...])}
+```
+
+## Limitations and Bias
+
+
+## Citation(s)
+
+```bibtex
+@article{HYNDMAN2006679,
+    title = {Another look at measures of forecast accuracy},
+    journal = {International Journal of Forecasting},
+    volume = {22},
+    number = {4},
+    pages = {679--688},
+    year = {2006},
+    issn = {0169-2070},
+    doi = {https://doi.org/10.1016/j.ijforecast.2006.03.001},
+    url = {https://www.sciencedirect.com/science/article/pii/S0169207006000239},
+    author = {Rob J. Hyndman and Anne B. Koehler},
+}
+```
+
+## Further References
+- [Mean absolute scaled error - Wikipedia](https://en.wikipedia.org/wiki/Mean_absolute_scaled_errorr)
---
+++ metrics/mase/mase.py
@@ -0,0 +1,142 @@
+# Copyright 2022 The HuggingFace Datasets Authors and the current dataset script contributor.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""MASE - Mean Absolute Scaled Error Metric"""
+
+import datasets
+import numpy as np
+from sklearn.metrics import mean_absolute_error
+
+import evaluate
+
+
+_CITATION = """\
+@article{HYNDMAN2006679,
+    title = {Another look at measures of forecast accuracy},
+    journal = {International Journal of Forecasting},
+    volume = {22},
+    number = {4},
+    pages = {679--688},
+    year = {2006},
+    issn = {0169-2070},
+    doi = {https://doi.org/10.1016/j.ijforecast.2006.03.001},
+    url = {https://www.sciencedirect.com/science/article/pii/S0169207006000239},
+    author = {Rob J. Hyndman and Anne B. Koehler},
+}
+"""
+
+_DESCRIPTION = """\
+Mean Absolute Scaled Error (MASE) is the mean absolute error of the forecast values, divided by the mean absolute error of the in-sample one-step naive forecast.
+"""
+
+
+_KWARGS_DESCRIPTION = """
+Args:
+    predictions: array-like of shape (n_samples,) or (n_samples, n_outputs)
+        Estimated target values.
+    references: array-like of shape (n_samples,) or (n_samples, n_outputs)
+        Ground truth (correct) target values.
+    training: array-like of shape (n_samples,) or (n_samples, n_outputs)
+        In sample training data for naive forecast.
+    periodicity: int, default=1
+        Seasonal periodicity of training data.
+    sample_weight: array-like of shape (n_samples,), default=None
+        Sample weights.
+    multioutput: {"raw_values", "uniform_average"} or array-like of shape (n_outputs,), default="uniform_average"
+        Defines aggregating of multiple output values. Array-like value defines weights used to average errors.
+
+                 "raw_values" : Returns a full set of errors in case of multioutput input.
+
+                 "uniform_average" : Errors of all outputs are averaged with uniform weight.
+
+Returns:
+    mase : mean absolute scaled error.
+        If multioutput is "raw_values", then mean absolute percentage error is returned for each output separately. If multioutput is "uniform_average" or an ndarray of weights, then the weighted average of all output errors is returned.
+        MASE output is non-negative floating point. The best value is 0.0.
+Examples:
+
+    >>> mase_metric = evaluate.load("mase")
+    >>> predictions = [2.5, 0.0, 2, 8, 1.25]
+    >>> references = [3, -0.5, 2, 7, 2]
+    >>> training = [5, 0.5, 4, 6, 3, 5, 2]
+    >>> results = mase_metric.compute(predictions=predictions, references=references, training=training)
+    >>> print(results)
+    {'mase': 0.18333333333333335}
+
+    If you're using multi-dimensional lists, then set the config as follows :
+
+    >>> mase_metric = evaluate.load("mase", "multilist")
+    >>> predictions = [[0, 2], [-1, 2], [8, -5]]
+    >>> references = [[0.5, 1], [-1, 1], [7, -6]]
+    >>> training = [[0.5, 1], [-1, 1], [7, -6]]
+    >>> results = mase_metric.compute(predictions=predictions, references=references, training=training)
+    >>> print(results)
+    {'mase': 0.18181818181818182}
+    >>> results = mase_metric.compute(predictions=predictions, references=references, training=training, multioutput='raw_values')
+    >>> print(results)
+    {'mase': array([0.10526316, 0.28571429])}
+    >>> results = mase_metric.compute(predictions=predictions, references=references, training=training, multioutput=[0.3, 0.7])
+    >>> print(results)
+    {'mase': 0.21935483870967742}
+"""
+
+
+@evaluate.utils.file_utils.add_start_docstrings(_DESCRIPTION, _KWARGS_DESCRIPTION)
+class Mase(evaluate.Metric):
+    def _info(self):
+        return evaluate.MetricInfo(
+            description=_DESCRIPTION,
+            citation=_CITATION,
+            inputs_description=_KWARGS_DESCRIPTION,
+            features=datasets.Features(self._get_feature_types()),
+            reference_urls=[
+                "https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mean_absolute_error.html"
+            ],
+        )
+
+    def _get_feature_types(self):
+        if self.config_name == "multilist":
+            return {
+                "predictions": datasets.Sequence(datasets.Value("float")),
+                "references": datasets.Sequence(datasets.Value("float")),
+            }
+        else:
+            return {
+                "predictions": datasets.Value("float"),
+                "references": datasets.Value("float"),
+            }
+
+    def _compute(
+        self,
+        predictions,
+        references,
+        training,
+        periodicity=1,
+        sample_weight=None,
+        multioutput="uniform_average",
+    ):
+
+        y_pred_naive = training[:-periodicity]
+        mae_naive = mean_absolute_error(training[periodicity:], y_pred_naive, multioutput=multioutput)
+
+        mae_score = mean_absolute_error(
+            references,
+            predictions,
+            sample_weight=sample_weight,
+            multioutput=multioutput,
+        )
+
+        epsilon = np.finfo(np.float64).eps
+        mase_scoe = mae_score / np.maximum(mae_naive, epsilon)
+
+        return {"mase": mase_scoe}
