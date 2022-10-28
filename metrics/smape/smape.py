6657e82ff4b422a4f6e7ea349969b7dee8d6c45d
Kashif Rasul <kashif.rasul@gmail.com>
2022-10-28 10:08:41 +0200

    formatting

--- metrics/smape/smape.py
+++ metrics/smape/smape.py
@@ -80,9 +80,7 @@ Examples:
 """
 
 
-def symmetric_mean_absolute_percentage_error(
-    y_true, y_pred, *, sample_weight=None, multioutput="uniform_average"
-):
+def symmetric_mean_absolute_percentage_error(y_true, y_pred, *, sample_weight=None, multioutput="uniform_average"):
     """Symmetric Mean absolute percentage error (sMAPE) metric.
 
     Parameters
@@ -110,16 +108,10 @@ def symmetric_mean_absolute_percentage_error(
         weighted average of all output errors is returned.
         sMAPE output is non-negative floating point. The best value is 0.0.
     """
-    y_type, y_true, y_pred, multioutput = _check_reg_targets(
-        y_true, y_pred, multioutput
-    )
+    y_type, y_true, y_pred, multioutput = _check_reg_targets(y_true, y_pred, multioutput)
     check_consistent_length(y_true, y_pred, sample_weight)
     epsilon = np.finfo(np.float64).eps
-    smape = (
-        2
-        * np.abs(y_pred - y_true)
-        / (np.maximum(np.abs(y_true), epsilon) + np.maximum(np.abs(y_pred), epsilon))
-    )
+    smape = 2 * np.abs(y_pred - y_true) / (np.maximum(np.abs(y_true), epsilon) + np.maximum(np.abs(y_pred), epsilon))
     output_errors = np.average(smape, weights=sample_weight, axis=0)
     if isinstance(multioutput, str):
         if multioutput == "raw_values":
@@ -154,9 +146,7 @@ class Smape(evaluate.Metric):
                 "references": datasets.Value("float"),
             }
 
-    def _compute(
-        self, predictions, references, sample_weight=None, multioutput="uniform_average"
-    ):
+    def _compute(self, predictions, references, sample_weight=None, multioutput="uniform_average"):
 
         smape_score = symmetric_mean_absolute_percentage_error(
             references,
