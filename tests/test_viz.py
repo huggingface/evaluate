1ec23526d2263c8cbefe2db2a97d36ea833f1d7b
sashavor <alexandra.vorobyova@gmail.com>
2022-11-08 17:13:05 -0500

    fixing a test (thanks Nima)

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -6,7 +6,7 @@ from evaluate.visualization import radar_plot
 
 
 class TestViz(TestCase):
-    def test_invert_range_bad_input(self):
+    def test_invert_range(self):
         data = [{"accuracy": 0.9, "precision": 0.8}, {"accuracy": 0.7, "precision": 0.6}]
         model_names = ["model1", "model2"]
         wrong_invert_range = ["latency_in_seconds"]  # Value not present in data
@@ -23,6 +23,12 @@ class TestViz(TestCase):
         out_plt = radar_plot(data, model_names, invert_range)
         self.assertIsInstance(out_plt, plt.Figure)
 
-    def test_input(self):
-        data = [{"accuracy": 0.9, "precision": 0.8}, {"accuracy": 0.7, "precision": 0.6}]
-        self.assertIsInstance(type(data), list)
+    def test_bad_input(self):
+        wrong_data = (
+            {"accuracy": 0.9, "precision": 0.8, "latency_in_seconds": 48.1},
+            {"accuracy": 0.7, "precision": 0.6, "latency_in_seconds": 51.4},
+        )  # Wrong data format (should be list)
+        model_names = ["model1", "model2"]
+        invert_range = ["latency_in_seconds"]
+        with self.assertRaises(ValueError):
+            radar_plot(wrong_data, model_names, invert_range)
