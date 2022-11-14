f9f423d848a5fa7d1fb8f43222af8a6cddc9b5d4
mathemakitten <helen.ngo14@gmail.com>
2022-11-14 14:51:52 -0800

    Test update

--- tests/test_evaluation_suite.py
+++ tests/test_evaluation_suite.py
@@ -10,10 +10,9 @@ class TestEvaluationSuite(TestCase):
     def test_running_evaluation_suite(self):
         results = self.evaluation_suite.run("lvwerra/distilbert-imdb")
 
-        # Check that the results are correct and task ids are generated as expected
-        for task_id, task in results.items():
-            if task == "imdb":
-                self.assertEqual(task["result"]["accuracy"], 1.0)
+        # Check that the results are correct
+        for r in results:
+            self.assertEqual(r["accuracy"], 1.0)
 
         # Check that correct number of tasks were run
-        self.assertEqual(len(list(results.keys())), 2)
+        self.assertEqual(len(results), 2)
