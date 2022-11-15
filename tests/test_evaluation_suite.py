0a70d1d91d928d6bd5f41401a1322041f304d987
mathemakitten <helen.ngo14@gmail.com>
2022-11-15 10:32:14 -0800

    Sub dummy model

--- tests/test_evaluation_suite.py
+++ tests/test_evaluation_suite.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from evaluate import EvaluationSuite
+from tests.test_evaluator import DummyTextClassificationPipeline
 
 
 class TestEvaluationSuite(TestCase):
@@ -9,14 +10,23 @@ class TestEvaluationSuite(TestCase):
         # Check that the EvaluationSuite loads successfully
         self.evaluation_suite = EvaluationSuite.load("evaluate/evaluation-suite-ci")
 
+        # Setup a dummy model for usage with the EvaluationSuite
+        self.dummy_model = DummyTextClassificationPipeline()
+
     def test_running_evaluation_suite(self):
 
         # Check that the evaluation suite successfully runs
-        results = self.evaluation_suite.run("lvwerra/distilbert-imdb")
+        results = self.evaluation_suite.run(self.dummy_model)
 
         # Check that the results are correct
         for r in results:
-            self.assertEqual(r["accuracy"], 1.0)
+            self.assertEqual(r["accuracy"], 0.5)
 
         # Check that correct number of tasks were run
         self.assertEqual(len(results), 2)
+
+    def test_empty_suite(self):
+
+        self.empty_suite = self.evaluation_suite
+        self.empty_suite.suite = []
+        self.assertRaises(ValueError, self.empty_suite.run, self.dummy_model)
