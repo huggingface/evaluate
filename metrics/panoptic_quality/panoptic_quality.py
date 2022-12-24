12fe7eed450719ddbd6e80691fd3645505967054
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-12-24 13:15:45 +0100

    Fix more code

--- metrics/panoptic_quality/panoptic_quality.py
+++ metrics/panoptic_quality/panoptic_quality.py
@@ -385,10 +385,9 @@ class PanopticQuality(evaluate.Metric):
 
             with io.BytesIO() as out:
                 seg_img.save(out, format="PNG")
-            
-            file_name = f"{image_id}.png"
-            with open(os.path.join(output_dir, file_name), "wb") as f:
-                f.write(out.getvalue())
+                file_name = f"{image_id}.png"
+                with open(os.path.join(output_dir, file_name), "wb") as f:
+                    f.write(out.getvalue())
 
         # step 2: create predictions JSON file
         # predicted_annotations is a list of segments_info
