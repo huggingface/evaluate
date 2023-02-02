b3457c5aee10a49db19ae78ab5dceec81e77b9f9
Tarık Kaan Koç <83168207+TarikKaanKoc@users.noreply.github.com>
2023-02-02 06:52:28 +0300

    Update metrics/r_squared/app.py

--- metrics/r_squared/app.py
+++ metrics/r_squared/app.py
@@ -1,6 +1,5 @@
 import evaluate
 from evaluate.utils import launch_gradio_widget
 
-
-module = evaluate.load("r_squared")
+module = evaluate.load("r_squared", module_type="metric")
 launch_gradio_widget(module)
