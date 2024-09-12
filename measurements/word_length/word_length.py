748b52b35ccd48d2b0b0f09cd418d01ee07b0605
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-09-12 09:03:49 +0200

    Fix import in word_length

--- measurements/word_length/word_length.py
+++ measurements/word_length/word_length.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from packaging import version
 from statistics import mean
 
 import datasets
