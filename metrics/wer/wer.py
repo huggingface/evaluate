c29a9dfddee48cb73d6788eff4cff9e12897c34c
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2025-06-20 14:53:23 +0200

    support jiwer 4.0

--- metrics/wer/wer.py
+++ metrics/wer/wer.py
@@ -14,7 +14,7 @@
 """ Word Error Ratio (WER) metric. """
 
 import datasets
-from jiwer import compute_measures
+import jiwer
 
 import evaluate
 
@@ -94,13 +94,25 @@ class WER(evaluate.Metric):
         )
 
     def _compute(self, predictions=None, references=None, concatenate_texts=False):
-        if concatenate_texts:
-            return compute_measures(references, predictions)["wer"]
+        if hasattr(jiwer, "compute_measures"):
+            if concatenate_texts:
+                return jiwer.compute_measures(references, predictions)["wer"]
+            else:
+                incorrect = 0
+                total = 0
+                for prediction, reference in zip(predictions, references):
+                    measures = jiwer.compute_measures(reference, prediction)
+                    incorrect += measures["substitutions"] + measures["deletions"] + measures["insertions"]
+                    total += measures["substitutions"] + measures["deletions"] + measures["hits"]
+                return incorrect / total
         else:
-            incorrect = 0
-            total = 0
-            for prediction, reference in zip(predictions, references):
-                measures = compute_measures(reference, prediction)
-                incorrect += measures["substitutions"] + measures["deletions"] + measures["insertions"]
-                total += measures["substitutions"] + measures["deletions"] + measures["hits"]
-            return incorrect / total
+            if concatenate_texts:
+                return jiwer.process_words(references, predictions).wer
+            else:
+                incorrect = 0
+                total = 0
+                for prediction, reference in zip(predictions, references):
+                    measures = jiwer.process_words(reference, prediction)
+                    incorrect += measures.substitutions + measures.deletions + measures.insertions
+                    total += measures.substitutions + measures.deletions + measures.hits
+                return incorrect / total
